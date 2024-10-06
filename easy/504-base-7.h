0b178558bf05f053682b22620984d1311a3c0975
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2024-10-06 13:44:50 +0300

    Add solution for 504. Base 7

---
+++ easy/504-base-7.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <bits/stdc++.h>
+#include "../problem_runner.h"
+
+// Given an integer num, return a string of its base 7 representation.
+
+DEFINE_PROBLEM(Base7, int, std::string) {
+    if (input == 0)
+        return "0";
+
+    char buffer[16];
+    size_t i = 0;
+
+    int32_t n = std::abs(input);
+    while (n != 0) {
+        buffer[16 - i++ - 1] = (n % 7) + '0';
+        n /= 7;
+    }
+
+    if (input < 0) {
+        buffer[16 - i++ - 1] = '-';
+    }
+
+    return std::string(buffer + 16 - i, i);
+})
+
+TEST_CASE(100, "202")
+TEST_CASE(-7, "-10")
+TEST_CASE(0, "0")
+TEST_CASE(42, "60")
+TEST_CASE(-42, "-60")
+TEST_CASE(1000000, "11333311")
+
+BUILD_PROBLEM;
