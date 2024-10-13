29f9661b5bca4b5b8eccc7e3ef6d02b4ee1f3995
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2024-10-13 14:41:31 +0300

    Move source files to src directory

--- main.cpp
+++
@@ -1,5 +0,0 @@
-#include "easy/405-convert-a-number-to-hexadecimal.h"
-
-int main() {
-    ConvertNumberToHexadecimal.run();
-}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+
+#include "medium/2191-sort-the-jumbled-numbers.h"
+
+int main() {
+    SortTheJumbledNumbers.run();
+}
