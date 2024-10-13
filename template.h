da5db9a5cc330043abd122b77edc2693b771979d
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2024-10-13 14:08:25 +0300

    Add template for problems

---
+++ template.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <bits/stdc++.h>
+#include "data/BinaryTree.h"
+#include "data/LinkedList.h"
+#include "problem_runner.h"
+
+DEFINE_PROBLEM(name, input, output) {
+
+})
+
+TEST_CASE(..., ...)
+
+BUILD_PROBLEM;
